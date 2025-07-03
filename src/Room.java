08b06d1891d5a99e25fee175aef0bf09242eb3b3
surapong <Peeraphat27277@gmail.com>
2025-07-04 00:43:06 +0700

    java_lab3_edit

--- src/Room.java
+++ src/Room.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
+
 public class Room {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("BOY COUNT:");
-        int Boy = scanner.nextInt();
-        System.out.print("GIRL COUNT:");
-        int Girl = scanner.nextInt();
-        Boy = (Boy/4)+1;
-        Girl = (Girl/3)+1;
-        int room = Boy + Girl;
-        System.out.println("Count room:"+room);
+
+        System.out.print("BOY COUNT: ");
+        int boy = scanner.nextInt();
+
+        System.out.print("GIRL COUNT: ");
+        int girl = scanner.nextInt();
+
+        int boyRoom = boy / 4;
+        if (boy % 4 > 0) {
+            boyRoom += 1;
+        }
+
+        int girlRoom = girl / 3;
+        if (girl % 3 > 0) {
+            girlRoom += 1;
+        }
+
+        int room = boyRoom + girlRoom;
+        System.out.println("Count room: " + room);
     }
 }
+
